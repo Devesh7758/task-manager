7ee9e9b9c2ac3665dec14d6bee78fc2b210819b2
Devesh7758 <deveshdwivedi1512@gmail.com>
2026-05-11 11:08:42 +0530

    Fixed backend URL typo

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const API = axios.create({
   baseURL:
-    "https://task-manager-a4on.onrender.com/api",
+    "https://task-manager-a40n.onrender.com/api",
 });
 
 export default API;
